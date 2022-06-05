14853d31418b84d310bd605f76ed37c0aa52d5b2
Denis <denelvis@yandex.ru>
2022-06-05 15:58:38 +0300

    added remark, date component

--- pages/posts/[slug].js
+++ pages/posts/[slug].js
@@ -1,8 +1,11 @@
+import Head from "next/head";
+import Date from "../../components/date";
 import Layout from "../../components/layout";
 import { getAllPostSlugs, getPostData } from "../../lib/posts";
+import utilStyles from "../../styles/utils.module.css";
 
 export async function getStaticProps({ params }) {
-  const postData = getPostData(params.slug);
+  const postData = await getPostData(params.slug);
   return {
     props: {
       postData,
@@ -21,11 +24,17 @@ export async function getStaticPaths() {
 export default function Post({ postData }) {
   return (
     <Layout>
-      {postData.title}
-      <br />
-      {postData.slug}
-      <br />
-      {postData.date}
+      <Head>
+        <title>{postData.title}</title>
+      </Head>
+      <article>
+        <h1 className={utilStyles.headingXl}>{postData.title}</h1>
+
+        <div className={utilStyles.ightText}>
+          <Date dateString={postData.date} />
+        </div>
+        <div dangerouslySetInnerHTML={{ __html: postData.contentHtml }} />
+      </article>
     </Layout>
   );
 }
